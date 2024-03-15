a6dc252e75dafeeb76917f3bd77aa60071ba2b85
NextGen-Code-Innovations <thellamaminecart.com@outlook.com>
2024-03-15 13:48:42 -0400

    save

--- frontEnd/lightControl.py
+++ frontEnd/lightControl.py
@@ -1,7 +1,13 @@
-import os 
+from os import system as cmd
 import time
 
-
-print("[0] Start server side things")
+print("[0] Reboot all network servers")
+print("[1] Start server side things")
 masterOption = int(input("Enter your option here: "))
-print(masterOption)
+
+if masterOption == 0:
+    print("test")
+elif masterOption == 1:
+    print("null test")
+else:
+    exit()
